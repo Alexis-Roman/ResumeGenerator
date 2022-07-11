98fe27d4f4bf814a60022cbedbc520ac9b8d6141
Alexis-Roman <104246596+Alexis-Roman@users.noreply.github.com>
2022-07-11 11:28:02 +0800

    still showing error

--- ResumeGenerator/Program.cs
+++ ResumeGenerator/Program.cs
@@ -1,3 +1,7 @@
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace ResumeGenerator
 {
     internal static class Program
@@ -8,10 +12,25 @@ namespace ResumeGenerator
         [STAThread]
         static void Main()
         {
+            static void Main(string[] args)
+            {
+                string filepath = GlobalVariable.resume;
+                var resumeJsonText = File.ReadAllText(filepath);
+                var resumedetails = JsonConvert.DeserializeObject<ResumeDetails>(resumeJsonText);
+            }
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new formResumeCreator());
         }
+    }
+    public class ResumeDetails
+    {
+        public string Name { get; set; }
+        public string LocationContactEmail { get; set; }
+        public string Objective { get; set; }
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public string Certification { get; set; }
     }
 }
